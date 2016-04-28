6e13859ab25ea69dc362aeac8e8885db53dfe5aa
Steve Fink <sfink@mozilla.com>
2016-04-28 15:09:03 -0700

    Bug 1259850 - Use decl_as_string to properly namespace-qualify types for C++

--- gcc/missing.c
+++ gcc/missing.c
@@ -6,11 +6,14 @@
 // we can use the plugin when compiling both C and CXX files. fortunately
 // when this plugin is loaded by cc1plus these will be ignored and the real
 // symbols called instead.
+//
+// decl_as_string is an exception. We *will* call this from C, so we provide a
+// fallback implementation here.
 
 EXTERN_BEGIN
 void lang_check_failed() { abort(); }
 const char * type_as_string(tree t, int x) { abort(); return NULL; }
-const char * decl_as_string(tree t, int x) { abort(); return NULL; }
+const char * decl_as_string(tree t, int x) { return IDENTIFIER_POINTER(DECL_NAME(t)); }
 int cp_type_quals(const_tree t) { abort(); return 0; }
 tree look_for_overrides_here(tree t, tree tt) { abort(); return NULL; }
 tree get_template_info(const_tree t) { abort(); return NULL; }
