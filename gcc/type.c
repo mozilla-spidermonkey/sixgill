4ca1c2750f5ec4eff1bd837945abe5a522b0e8f4
Steve Fink <sfink@mozilla.com>
2019-07-15 17:51:25 -0700

    Attempted hack to fix gcc 7 crash ("no class template named _CharT in std::__detail::_RegexTranslator<_TraitsT, __icase, __collate>")

--- gcc/type.c
+++ gcc/type.c
@@ -320,7 +320,15 @@ const char* XIL_CSUName(tree type, const char *name)
 
   // template instantiations do not have names filled in. get one now.
   if (c_dialect_cxx() && !*field_csu && CLASS_TYPE_P(type) && CLASSTYPE_USE_TEMPLATE(type)) {
+#if GCC_VERSION_NUMBER >= 70000 && GCC_VERSION_NUMBER < 80000
+    // gcc 7.3.0 has a bug that will crash on converting a certain template
+    // parameter to a string. Not chasing typedefs will make template
+    // instantiations have somewhat bogus types and so may not match other
+    // instantiations of the same type.
+    const char *new_name = type_as_string(type, 0);
+#else
     const char *new_name = type_as_string(type, TFF_CHASE_TYPEDEF);
+#endif
     *field_csu = XIL_TypeCSU(new_name, NULL);
   }
 
